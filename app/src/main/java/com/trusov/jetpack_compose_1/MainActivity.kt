41caac3513daecc66da15e20872fbda64e1c628d
Glebasta333 <gleb.trusow@yandex.ru>
2022-04-11 11:10:46 +0300

    add text with decoration

--- app/src/main/java/com/trusov/jetpack_compose_1/MainActivity.kt
+++ app/src/main/java/com/trusov/jetpack_compose_1/MainActivity.kt
@@ -15,11 +15,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextGeometricTransform
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -30,11 +34,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val message: MutableState<String> = remember{mutableStateOf("Hello Jetpack")}
+            val message: MutableState<String> = remember { mutableStateOf("Hello Jetpack") }
             Text(
                 text = message.value,
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { message.value = "Hello Work!" })
+                color = Color.DarkGray,
+                fontSize = 40.sp,
+                overflow = TextOverflow.Ellipsis,
+                fontFamily = FontFamily.Cursive,
+                style = TextStyle(
+                    textGeometricTransform = TextGeometricTransform(scaleX = 2f),
+                    shadow = Shadow(
+                        color = Color.LightGray,
+                        offset = Offset(19.0f, 20.0f),
+                        blurRadius = 2.0f
+                    )
+                ),
+                modifier = Modifier.clickable(onClick = { message.value = "Hello Work! Hello Work! Hello Work!" })
+                    .padding(32.dp)
+                    .background(Color.Yellow)
+                    .padding(32.dp)
             )
         }
     }
