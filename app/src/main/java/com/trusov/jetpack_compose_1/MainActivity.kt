ccf8c2e829b5e21419be5c1a36b4bb091de8249f
Glebasta333 <gleb.trusow@yandex.ru>
2022-04-28 14:32:28 +0300

    add scaffold and drawer

--- app/src/main/java/com/trusov/jetpack_compose_1/MainActivity.kt
+++ app/src/main/java/com/trusov/jetpack_compose_1/MainActivity.kt
@@ -6,17 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -35,54 +32,31 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.trusov.jetpack_compose_1.ui.theme.Jetpack_compose_1Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val colors = listOf(Color.Red, Color.Green, Color.Blue)
-            val selectedOption = remember { mutableStateOf(colors[0]) }
-            Column(modifier =Modifier.padding(20.dp)) {
-                Box( Modifier.padding(10.dp).size(100.dp).background(color = selectedOption.value))
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
 
-                colors.forEach { color ->
-                    val selected = selectedOption.value == color
-                    Box(
-                        Modifier.padding(8.dp)
-                            .size(60.dp)
-                            .background(color = color)
-                            .selectable(
-                                selected = selected,
-                                onClick = { selectedOption.value = color }
-                            )
-                            .border(
-                                width= if(selected){2.dp} else{0.dp},
-                                color = Color.Black
-                            )
-                    )
+            Scaffold(
+                scaffoldState = scaffoldState,
+                drawerContent={
+                    Text("Пункт меню 1", fontSize = 28.sp)
+                    Text("Пункт меню 2", fontSize = 28.sp)
+                    Text("Пункт меню 3", fontSize = 28.sp)
+                }
+            ){
+                Button(onClick = {
+                    scope.launch{
+                        scaffoldState.drawerState.open()
+                    }
+                }) {
+                    Text("Меню", fontSize = 28.sp)
                 }
             }
-
-
         }
-    }
-
-    private fun setBoxColor(checkedState: Boolean): Color {
-        return if (checkedState) Color.Red else Color.Blue
-    }
-
-}
-
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Jetpack_compose_1Theme {
-        Greeting("Android")
     }
 }
